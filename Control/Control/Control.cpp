f8334bcdd4b9220f644db7a8c33773b08519f28c
daniel.shama@gmail.com <daniel.shama@gmail.com>
2016-06-23 11:04:07 +0300

    impl. control class

--- Control/Control/Control.cpp
+++ Control/Control/Control.cpp
@@ -1,11 +1,131 @@
 #include "Control.h"
 
 
-Control::Control()
-{
+Control::Control(int _width) : 
+	width(_width), isFocused(false), isfocusable(false), visible(true), height(1){}
+
+void Control::show(){
+	visible = true;
+}
+void Control::hide(){
+	visible = false;
+}
+void Control::focus(){
+	isFocused = true;
+}
+void Control::unfocus(){
+	isFocused = false;
+}
+void Control::setForeground(ForegroundColor color){
+	foreground = color;
+}
+void Control::setBackGround(BackgroundColor color){
+	background = color;
+}
+void Control::setBorder(BorderType type){
+	borderType = type;
+}
+BorderType Control::getBorderType(){
+	return borderType;
+}
+char Control::getBorderTypeHorizontal(){
+	switch (getBorderType())
+	{
+	case BorderType::Single:
+			return '-';
+			break;
+	case BorderType::Double:
+		return '=';
+		break;
+	default:
+		return NULL;
+		break;
+	}
+}
+char Control::getBorderTypeVertical(){
+	switch (getBorderType())
+	{
+	case BorderType::Single:
+		return '|';
+		break;
+	case BorderType::Double:
+		return 186; // ' || '
+		break;
+	default:
+		return NULL;
+		break;
+	}
+}
+void Control::setLocation(COORD c){
+	location = c;
+}
+void Control::draw(Graphics graphics, int x, int y, size_t w){
+	width += 2;
+	string str(width, getBorderTypeHorizontal());
+	graphics.write(getLeft(), getTop(), str);
+	
+	for (int i = 0; i < height; i++){
+		string strv(width + 2, ' ');
+		strv[0] = getBorderTypeVertical();
+		strv[height] = getBorderTypeVertical();
+		graphics.write(getLeft(), (getTop() + 1 ) + i, strv);
+	}
+
+	height += 2;
+
+	graphics.write(location.X, location.Y + (height+1), str);
 }
 
+void Control::keydown(WORD, CHAR){}
 
-Control::~Control()
-{
+void Control::mousePressed(short, short, DWORD){}
+
+bool Control::canGetFocus(){
+	return isfocusable;
+}
+bool Control::validSpace(Control c){
+	//take care for the border
+	if ((c.getLeft() + c.getWidth()) < getLeft() ||
+		(getLeft() + getWidth()) < c.getLeft()) {
+		return true;
+	}
+	else if ( (c.getTop() + c.getHeight() ) > getTop() ||
+		(getTop() + getHeight()) < c.getTop()) {
+		return true;;
+	}
+	return false;
+}
+bool Control::isFocus(){
+	return isFocused;
+}
+bool Control::isVisible(){
+	return visible;
+}
+short Control::getLeft(){
+	return location.Y;
+}
+short Control::getTop(){
+	return location.X;
+}
+
+int Control::getWidth(){
+	return width;
+}
+int Control::getHeight(){
+	return height;
+}
+
+vector<Control*> Control::getAllControls(){
+	return vector<Control*>();
+}
+
+void Control::setFocus(Control* c){
+	currentFocused->unfocus();
+	c->focus();
+}
+Control* Control::getFocus(){
+	return currentFocused;
+}
+Control::~Control(){
+
 }
