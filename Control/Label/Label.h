b89e238091d031a4b32fd9266e02f3dae60a724b
noamroiz <noam@datorama.com>
2016-06-30 19:37:07 +0300

    Finished the numeric box, value is now in the middle.

--- Control/Label/Label.h
+++ Control/Label/Label.h
@@ -11,7 +11,7 @@ public:
 
 	void setValue(string value);
 	void draw(Graphics graphics, int, int, size_t);
-	string makeStringInTheMiddle(int width, string value);
+	static string makeStringInTheMiddle(int width, string value);
 	string getValue();
 	void keyDown(WORD, CHAR) {};
 	void mousePressed(short, short, bool) {};
--- Control/NumericBox/NumericBox.cpp
+++ Control/NumericBox/NumericBox.cpp
@@ -21,9 +21,9 @@ NumericBox::NumericBox(int width, int min, int max):Panel(5,width)
 	plusBtn->addListener(*plusListener);
 	
 	addControl(minusBtn, getBodyLeft(), getBodyTop());
-	addControl(labelVal, getBodyLeft() +((width-8)/2), getBodyTop());
-	addControl(plusBtn, labelVal->getLeft() + labelVal->getWidth(), getBodyTop());
 	this->min = min;
+	addControl(labelVal, getBodyLeft() + minusBtn->getWidth(), getBodyTop());
+	addControl(plusBtn, labelVal->getLeft() + labelVal->getWidth(), getBodyTop());
 	this->max = max;
 }
 
@@ -36,10 +36,17 @@ void NumericBox::setValue(int val) {
 	
 	if ( val < min || val >max) return;
 	else {
-		static_cast<Label*>(controls[1])->setValue(to_string(val));
+		static_cast<Label*>(controls[1])->setValue(static_cast<Label *>(controls[1])->makeStringInTheMiddle(controls[1]->getWidth()-1, to_string(val)));
 	}
 };
 int NumericBox::getValue() {	
 	string val = static_cast<Label*>(controls[1])->getValue();
-	return stoi(val);
+	int size = val.length();
+	string num;
+	for (int i = 0; i < size; i++) {
+		if (val.at(i) != '\0') {
+			num += val.at(i);
+		}
+	}
+	return stoi(num);
 };
